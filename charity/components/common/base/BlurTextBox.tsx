c8c47d9fe58f0ca795a5f76b56876270f343359c
mtighorbani <mahdi1381.ghorbani@gmail.com>
2024-11-11 18:00:34 +0330

    feat: make base blur text box

---
+++ charity/components/common/base/BlurTextBox.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import getClassNames from "@/utils/getClassName";
+
+interface BlurTextProps {
+  className?: string;
+  textClassName?: string;
+  text: string;
+}
+const BlurTextBox = ({ className, text, textClassName }: BlurTextProps) => {
+  return (
+    <div
+      className={getClassNames([
+        " max-w-[30%] backdrop-blur-xl min-h-12 py-4 px-7 rounded-lg text-center ",
+        className,
+      ])}
+    >
+      <p className={getClassNames([" text-2xl font-extrabold", textClassName])}>
+        {text}
+      </p>
+    </div>
+  );
+};
+
+export default BlurTextBox;
