7615761f3034d630c2291e5d896901118460feb8
mtighorbani <mahdi1381.ghorbani@gmail.com>
2024-12-16 15:05:18 +0330

    update packages

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
+module.exports = {
+  productionBrowserSourceMaps: true,
+}
+module.exports = {
+  presets: ["next/babel"],
+  plugins: [
+    ["@locator/babel-jsx/dist", {
+      env: "development",
+    }]
+  ]
+};
 
-export default nextConfig;
+export default nextConfig
